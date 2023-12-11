01543bd140565932f6377337c79f500fa78fa7e1
Aon Muhammad <145316686+AonAideveloper@users.noreply.github.com>
2023-12-11 09:50:07 +0500

    Add files via upload

---
+++ pratice 3.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 20:23:21 2023
+
+@author: web 96
+"""
+
+##Boolean expression 
+
+# 3 == 3
+# 3 == 2
+
+# x = 5
+# y = 6
+
+# print('x =' ,x, 'y = ',y)
+# print('checking less than with \'<\':',x < y)
+# print('Checking greater than with \'>\':',x > y)
+
+# var_1 = 7
+# var_2 = 7
+# print('var_1 =',var_1,'var_2 = ',var_2)
+# print('Checking equality with \'==\':',var_1 == var_2)
+# print('Checking not equal with \'!=\':',var_1 <= var_2)
+# print('Checking less than or equal with \'<=\':', var_1<= var_2)
+# print('Checking greater than or equal with \'>=\':',var_1 >= var_2)
+
+
+# a = 200
+# b = 33
+
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
